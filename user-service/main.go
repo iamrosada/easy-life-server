f83ff90ec12f7449e8b28a086b3ac3b6ab94da7e
iamrosada <luisrosada2018@gmail.com>
2023-12-09 14:05:55 +0300

    feat:  main function

---
+++ user-service/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	// "github.com/iamrosada/easy-life-server/Student-server/internal/entity"
+	// student "github.com/iamrosada/easy-life-server/class-service/internal/usecase/student"
+	// teacher "github.com/iamrosada/easy-life-server/class-service/internal/usecase/teacher"
+
+	"github.com/iamrosada/easy-life-server/user-server/api"
+	"github.com/iamrosada/easy-life-server/user-server/internal/entity"
+	repository "github.com/iamrosada/easy-life-server/user-server/internal/infra"
+	"github.com/iamrosada/easy-life-server/user-server/internal/usecase/student"
+	"github.com/iamrosada/easy-life-server/user-server/internal/usecase/teacher"
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dbPath := "./db/main.db"
+	sqlDB, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		panic(err)
+	}
+	defer sqlDB.Close()
+
+	_, err = os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+	}
+
+	// Create Gorm connection
+	gormDB, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = gormDB.AutoMigrate(&entity.Student{}, &entity.Teacher{})
+	if err != nil {
+		panic(err)
+	}
+	// Create repositories and use cases
+	StudentRepository := repository.NewStudentRepositoryPostgres(gormDB)
+	createStudentUsecase := student.NewCreateStudentUseCase(StudentRepository)
+	listStudentsUsecase := student.NewGetAllStudentUseCase(StudentRepository)
+	deleteStudentUsecase := student.NewDeleteStudentUseCase(StudentRepository)
+	getStudentByIDUsecase := student.NewGetStudentByIDUseCase(StudentRepository)
+	updateStudentUsecase := student.NewUpdateStudentUseCase(StudentRepository)
+
+	// Create handlers
+	StudentHandlers := api.NewStudentHandlers(createStudentUsecase, listStudentsUsecase, deleteStudentUsecase, getStudentByIDUsecase, updateStudentUsecase)
+
+	// Set up Gin router
+	router := gin.Default()
+
+	TeacherRepository := repository.NewTeacherRepositoryPostgres(gormDB)
+	createTeacherUsecase := teacher.NewCreateTeacherUseCase(TeacherRepository)
+	listTeachersUsecase := teacher.NewGetAllTeacherUseCase(TeacherRepository)
+	deleteTeacherUsecase := teacher.NewDeleteTeacherUseCase(TeacherRepository)
+	getTeacherByIDUsecase := teacher.NewGetTeacherByIDUseCase(TeacherRepository)
+	updateTeacherUsecase := teacher.NewUpdateTeacherUseCase(TeacherRepository)
+
+	TeacherHandlers := api.NewTeacherHandlers(createTeacherUsecase, listTeachersUsecase, deleteTeacherUsecase, getTeacherByIDUsecase, updateTeacherUsecase)
+
+	// Set up Student routes
+	StudentHandlers.SetupRoutes(router)
+	TeacherHandlers.SetupRoutes(router)
+
+	// Start the server
+	err = http.ListenAndServe(":8000", router)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
