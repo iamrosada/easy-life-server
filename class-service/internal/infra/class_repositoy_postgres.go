c71f71f9049211aa244596ee2e8591206d2b47c3
iamrosada <luisrosada2018@gmail.com>
2023-12-10 12:00:49 +0300

    feat: apply event from onetherr api is done

--- class-service/internal/infra/class_repositoy_postgres.go
+++ class-service/internal/infra/class_repositoy_postgres.go
@@ -1,8 +1,10 @@
 package repository
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/iamrosada/easy-life-server/class-service/internal/entity"
 	_ "gorm.io/driver/postgres"
@@ -26,12 +28,46 @@ func (r *ClassRepositoryPostgres) Create(class *entity.Class) error {
 		TeacherID:     class.TeacherID,
 		StudentsIDs:   class.StudentsIDs,
 	}
-	// []string{"vida", "Amor"}
-	if err := r.DB.Create(&createdNewClass).Error; err != nil {
-		// log.Printf("Error creating class for user %s: %v", studentID, err)
-		// return fmt.Errorf("failed to create class for user %s: %v", studentID, err)
 
+	if err := r.DB.Create(&createdNewClass).Error; err != nil {
+		return fmt.Errorf("failed to create class: %v", err)
 	}
+
+	// Call the external API with event ID and student IDs
+	if err := r.callExternalAPI(class.ID, class.StudentsIDs); err != nil {
+		// Handle the error as needed
+		return fmt.Errorf("failed to call external API: %v", err)
+	}
+
+	return nil
+}
+
+func (r *ClassRepositoryPostgres) callExternalAPI(eventID string, studentIDs []string) error {
+	apiURL := fmt.Sprintf("http://localhost:8000/api/students/event/%s", eventID)
+
+	// Create a map for the request body
+	requestBody := map[string]interface{}{
+		"students_ids": studentIDs,
+	}
+
+	// Convert the map to JSON
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON: %v", err)
+	}
+
+	// Make a POST request to the external API
+	resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return fmt.Errorf("failed to make POST request to external API: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response status
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected response status from external API: %v", resp.Status)
+	}
+
 	return nil
 }
 
